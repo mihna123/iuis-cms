88b7a4e3fa969c324efd2ef523bca5dbf7748c77
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-03-25 18:59:58 +0100

    Add table to table form

--- TableForm.cs
+++ TableForm.cs
@@ -6,11 +6,54 @@ namespace CMSystem
 {
     public class TableForm : Form
     {
-
+        private DataGridView gridView = new DataGridView();
+        private IOService ioService = new IOService();
+        private List<Device> devices = new List<Device>();
 
         public TableForm()
         {
-            this.Text = "Welcome to Control Managenemt Systems!";
+            this.Text = "Control Managenemt Systems!";
+            devices = ioService.LoadDevices();
+            SetupTable();
+        }
+
+        private void SetupTable()
+        {
+            this.Controls.Add(gridView);
+
+            gridView.ColumnCount = 5;
+            gridView.Name = "Devices View";
+            gridView.Location = new Point(8, 8);
+            gridView.Size = new Size(600, 800);
+            gridView.GridColor = Color.Black;
+            var checkboxColumn = new DataGridViewCheckBoxColumn();
+
+            checkboxColumn.HeaderText = "Select";
+            checkboxColumn.Name = "Select";
+            checkboxColumn.AutoSizeMode =
+                DataGridViewAutoSizeColumnMode.DisplayedCells;
+            checkboxColumn.FlatStyle = FlatStyle.Standard;
+            checkboxColumn.ThreeState = true;
+            checkboxColumn.CellTemplate = new DataGridViewCheckBoxCell();
+            checkboxColumn.CellTemplate.Style.BackColor = Color.Beige;
+
+            gridView.Columns.Insert(0, checkboxColumn);
+            gridView.Columns[0].Name = "Select";
+            gridView.Columns[1].Name = "ID";
+            gridView.Columns[2].Name = "Name";
+            gridView.Columns[3].Name = "Path";
+            gridView.Columns[4].Name = "Creation Date";
+
+            foreach (var dev in devices)
+            {
+                var row = new List<string>();
+                row.Add(dev.ID.ToString());
+                row.Add(dev.Name);
+                row.Add(dev.RichTextPath);
+                row.Add(dev.CreationDate.ToString());
+
+                gridView.Rows.Add(row.ToArray());
+            }
         }
 
         private void SetupLayout()
@@ -18,3 +61,9 @@ namespace CMSystem
         }
     }
 }
+
+// This is to make it shaped cool
+//this.FormBorderStyle = FormBorderStyle.None;
+//this.BackgroundImage = Image.FromFile("shape.bmp");
+//this.ClientSize = this.BackgroundImage.Size;
+//this.TransparencyKey = Color.White;
