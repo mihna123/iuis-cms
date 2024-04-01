784f83745602efc76cc3b551282a912c87dc689e
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-04-01 21:41:41 +0200

    Upgrade user view of a device

--- ViewDeviceForm.cs
+++ ViewDeviceForm.cs
@@ -22,18 +22,50 @@ namespace CMSystem
         private void SetupLayout()
         {
             this.Controls.Add(layout);
-            this.layout.Size = new Size(450, 300);
+            this.layout.Size = new Size(500, 600);
             this.layout.WrapContents = true;
             this.layout.FlowDirection = FlowDirection.LeftToRight;
+            this.layout.Padding = new Padding(10);
 
+            var dataLayout = new FlowLayoutPanel();
+            dataLayout.Size = new Size(310, 200);
+            dataLayout.WrapContents = true;
+            dataLayout.FlowDirection = FlowDirection.LeftToRight;
+
+            var img = new PictureBox();
             var lblName = new Label();
             var lblDate = new Label();
+            var lblDesc = new Label();
+            var rtbDesc = new RichTextBox();
 
-            lblName.Text = string.Format("Name: {0}", this.device.Name);
-            lblName.Text = string.Format("Created Date: {0}",
+            img.SizeMode = PictureBoxSizeMode.StretchImage;
+            img.Image = Image.FromFile(device.ImagePath);
+            img.ClientSize = new Size(150, 150);
+
+            lblDate.Text = string.Format("Created Date: {0}",
                                           this.device.CreationDate);
-            this.layout.Controls.Add(lblName);
-            this.layout.Controls.Add(lblDate);
+            lblName.Text = string.Format("Name: {0}", this.device.Name);
+            lblDesc.Text = "Description: ";
+
+            lblName.Width = 300;
+            lblDate.Width = 300;
+            lblDesc.Width = 300;
+
+            rtbDesc.LoadFile(device.RichTextPath);
+            rtbDesc.ReadOnly = true;
+            rtbDesc.Size = new Size(300, 50);
+
+            goBackBtn.Text = "Go back";
+            goBackBtn.Click += new EventHandler((o, e) => this.Close());
+
+            dataLayout.Controls.Add(lblName);
+            dataLayout.Controls.Add(lblDate);
+            dataLayout.Controls.Add(lblDesc);
+            dataLayout.Controls.Add(rtbDesc);
+
+            this.layout.Controls.Add(img);
+            this.layout.Controls.Add(dataLayout);
+            this.layout.Controls.Add(goBackBtn);
         }
     }
 }
